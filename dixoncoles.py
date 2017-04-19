fb0f7200512bf66bc82ef7fcbe14ce6bfbb8752c
anguswilliams91 <anguswilliams91@gmail.com>
2017-04-19 12:55:03 +0100

    tidy up

--- dixoncoles.py
+++ dixoncoles.py
@@ -1,4 +1,6 @@
 from __future__ import division, print_function
+import warnings
+import os
 
 import pandas as pd 
 import numpy as np 
@@ -11,7 +13,23 @@ from scipy.misc import factorial
 from multiprocessing import pool
 
 def process_raw_data(raw,filestr='data/PL_2015-16.csv'):
-    """http://www.football-data.co.uk/englandm.php"""
+    """Data downloaded from http://www.football-data.co.uk/englandm.php
+
+    Arguments
+    ---------
+
+    raw: pandas.DataFrame
+        The data as downloaded from the above url, loaded into pandas.
+
+    filestr: (='data/PL_2015-16.csv') string
+        Location to save the modified file at.
+
+    Returns
+    -------
+
+    data: pandas.DataFrame
+        The data indexed by date and with columns for home and away teams, home and 
+        away goals."""
 
     raw.loc[:,'Date'] = pd.to_datetime(raw.loc[:,'Date'],dayfirst=True)
     raw.rename(columns={'FTHG':'HomeGoals','FTAG':'AwayGoals'},inplace=True)
@@ -23,6 +41,25 @@ def process_raw_data(raw,filestr='data/PL_2015-16.csv'):
 
 def load_and_preprocess():
 
+    """
+    Load the data and do some pre-processing. The data are 
+    presumed to be located at "data/PL_2015-16.csv".
+
+    Returns
+    -------
+
+    data: pandas.DataFrame
+        The data, with an extra 'Gameweek' column added 
+        for convenience, and teams replaced by integer 
+        identifiers.
+
+    teams: array_like
+        List of teams such that the integer identifier for 
+        teams[i] is i. 
+
+
+    """
+
     data = pd.read_csv("data/PL_2015-16.csv")
     teams = pd.unique(data['HomeTeam'])
     for i,team in enumerate(teams):
@@ -32,7 +69,6 @@ def load_and_preprocess():
         data.loc[away_idx,'AwayTeam'] = i
     data[['HomeTeam','AwayTeam']] = data[['HomeTeam','AwayTeam']].apply(pd.to_numeric)
 
-    #add a 'round' column by grouping by week
     data = data.set_index('Date')
     data.index = pd.to_datetime(data.index)
     data['Gameweek'] = data.index.year * 100 + data.index.week
@@ -43,6 +79,33 @@ def load_and_preprocess():
     return data,teams
 
 def compute_lambdas_mus(params,data,n_teams):
+
+    """
+    Compute the rates for the home team and away 
+    team scoring.
+
+    Arguments
+    ---------
+
+    params: array_like
+        The model parameters.
+
+    data: pandas.DataFrame
+        The data.
+
+    n_teams: int
+        The number of teams in the league (20 for the PL).
+
+    Returns
+    -------
+
+    mu_k: array_like
+        The rates for the away teams. 
+
+    lambda_k: array_like
+        The rates for the home teams.
+
+    """
 
     alphas = params[: n_teams-1 ]
     betas = params[n_teams-1 : 2*n_teams - 1]
@@ -63,7 +126,39 @@ def compute_lambdas_mus(params,data,n_teams):
 
     return mu_k,lambda_k
 
-def log_likelihood(params,data,n_teams=20,zeta=0.0,date=None):
+def log_likelihood(params,data,n_teams=20,zeta=0.003,date=None):
+
+    """
+    Compute the (negative) log likelihood for a given set of model 
+    parameters.
+
+    Arguments
+    ---------
+
+    params: array_like
+        The model parameters.
+
+    data: pandas.DataFrame
+        The data.
+
+    n_teams: (=20) int
+        The number of teams in the league (20 for the PL).
+
+    zeta: float 
+        The hyperparameter that determines the importance of 
+        matches at time t in the past. The rate is in 1/days 
+        rather than 1/half-weeks as is done in Dixon and Coles.
+
+    date: string or datetime object 
+        The date for which the fit should be carried out.
+
+    Returns
+    -------
+
+    lnL: float
+        The negative log-likelihood. 
+
+    """
 
     rho = params[-1]
     x_k = data['HomeGoals'].values.astype('float') 
@@ -84,25 +179,121 @@ def log_likelihood(params,data,n_teams=20,zeta=0.0,date=None):
     if date is None:
         prediction_date = data.index.values[-1]
     else:
-        prediction_date = pd.to_datetime(date)
-    dt = np.array([pd.Timedelta(prediction_date - mdi).days/7. for mdi in match_dates])
+        prediction_date = np.datetime64(pd.to_datetime(date))
+    dt = (prediction_date - match_dates).astype('timedelta64[D]')
 
-    return -np.sum( (-mu_k + y_k*np.log(mu_k) - lambda_k + x_k*np.log(lambda_k) + np.log(correlations))*np.exp(-dt*zeta))
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore",category=RuntimeWarning)
+        lnL = -np.sum( (-mu_k + y_k*np.log(mu_k) - lambda_k + x_k*np.log(lambda_k) + np.log(correlations))*np.exp(-dt.astype(float)*zeta) )
+
+    return lnL
 
 def rho_constraint_low(params,data,n_teams):
 
-    mu_k,lambda_k = compute_lambdas_mus(params,data,n_teams)
-    lower_bound = np.max(np.hstack((-1./lambda_k,-1./mu_k)))
+    """
+    The lower bound on the correlation parameter from 
+    Dixon and Coles.
+
+    Arguments
+    ---------
+
+    params: array_like
+        The model parameters.
+
+    data: pandas.DataFrame
+        The data.
+
+    n_teams: int 
+        The number of teams in the league.
+
+    Returns
+    -------
+
+    bound: float
+        rho - lower_bound, such that bound > 0 must be satisfied.
+
+    """
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore",category=RuntimeWarning)
+        mu_k,lambda_k = compute_lambdas_mus(params,data,n_teams)
+        lower_bound = np.max(np.hstack((-1./lambda_k,-1./mu_k)))
     return params[-1] - lower_bound
 
 def rho_constraint_high(params,data,n_teams):
 
-    mu_k,lambda_k = compute_lambdas_mus(params,data,n_teams)
-    upper_bound = np.min(np.append((lambda_k*mu_k)**-1.,1.))
+    """
+    The upper bound on the correlation parameter from 
+    Dixon and Coles.
+
+    Arguments
+    ---------
+
+    params: array_like
+        The model parameters.
+
+    data: pandas.DataFrame
+        The data.
+
+    n_teams: int 
+        The number of teams in the league.
+
+    Returns
+    -------
+
+    bound: float
+        upper_bound - rho, such that bound > 0 must be satisfied.
+
+    """
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore",category=RuntimeWarning)
+        mu_k,lambda_k = compute_lambdas_mus(params,data,n_teams)
+        upper_bound = np.min(np.append((lambda_k*mu_k)**-1.,1.))
     return upper_bound - params[-1]
 
 
-def compute_parameters(data,params_guess=None,n_teams=20,date=None,zeta=0.):
+def compute_parameters(data,params_guess=None,n_teams=20,date=None,zeta=0.003):
+
+    """
+    Compute the parameters of the model given some data.
+
+    Arguments
+    ---------
+
+    data: pandas.DataFrame
+        The data.
+
+    params_guess: (= None) array_like
+        Guesses for the model parameters. If None, then all the parameters 
+        are set to 1 except for rho, which is set to 0.
+
+    n_teams: (=20) int
+        The number of teams in the league (20 for the PL).
+
+    date: (=None) string or datetime object 
+        The date for which the fit should be carried out.
+
+    zeta: (=0.003) float 
+        The hyperparameter that determines the importance of 
+        matches at time t in the past. The rate is in 1/days 
+        rather than 1/half-weeks as is done in Dixon and Coles.
+
+    Returns
+    -------
+
+    alphas: array_like
+        The attacking indices for each team.
+
+    betas: array_like
+        The defending indices for each team.
+
+    gamma: float
+        The home advantage parameter.
+
+    rho: float
+        The correlation parameter for low-scoring 
+        games. 
+
+    """
 
     if params_guess is None:
         params_guess = np.ones(2*n_teams + 1)
@@ -119,8 +310,7 @@ def compute_parameters(data,params_guess=None,n_teams=20,date=None,zeta=0.):
             method='SLSQP', bounds=bnds, constraints=cons, options={'maxiter': 200})
 
     if res.success != True:
-        print('Failed to optimize the model.')
-        return res
+        Exception('Failed to optimize the model.')
 
     alphas = res.x[:n_teams - 1]
     alphas = np.append(alphas, n_teams - np.sum(alphas))
@@ -134,13 +324,46 @@ def compute_parameters(data,params_guess=None,n_teams=20,date=None,zeta=0.):
 
 def fit_model_up_to_round(data,gameweek=1):
 
+    """
+    Fit the model up to a given 'round', defined by the Gameweek column 
+    in the data file. Instead of using exp(-zeta * t), a simple sliding 
+    window function is used where the ten previous gameweeks are considered.
+
+    Arguments
+    ---------
+
+    data: pandas.DataFrame
+        The data.
+
+    gameweek: (=1) int
+        The gameweek (1<= gameweek <= 36) up to which the model should be fit. Only the ten 
+        weeks before the selected gameweek will be considered.
+
+    Returns
+    -------
+
+    alphas: array_like
+        The attacking indices for each team.
+
+    betas: array_like
+        The defending indices for each team.
+
+    gamma: float
+        The home advantage parameter.
+
+    rho: float
+        The correlation parameter for low-scoring 
+        games. 
+
+    """
+
     params = np.ones(41)
     params[-1] = 0.
     if gameweek<10:
         train = data.loc[data['Gameweek'] <= gameweek, :]
     else:
         train = data.loc[(data['Gameweek'] <= gameweek)&(data['Gameweek'] > gameweek - 10), :]
-    res = compute_parameters(train,params_guess=params,n_teams=20)
+    res = compute_parameters(train,params_guess=params,n_teams=20,zeta=0.)
     try:
         alphas,betas,gamma,rho = res
         return alphas,betas,gamma,rho
@@ -149,12 +372,47 @@ def fit_model_up_to_round(data,gameweek=1):
 
 def fit_model_season(data):
 
-    alpha_tot = np.zeros((62,20))
-    beta_tot = np.zeros((62,20))
-    gamma_tot = np.zeros(62)
-    rho_tot = np.zeros(62)
+    """
+    Fit the model for the whole season, computing the parameters for each gameweek. Note 
+    that the first ~ 5 gameweeks should really be disregarded because the parameter estimates 
+    are fluctuating rapidly because of a lack of historical data.
 
-    for i in np.arange(1,63):
+    Arguments
+    ---------
+
+    data: pandas.DataFrame
+        The data.
+
+    Returns
+    -------
+
+    alphas: array_like
+        The attacking indices for each team. The shape of this 
+        array will be [n_gameweeks, n_teams] such that alphas[i,j] 
+        gives the attacking index of team j in gameweek i.
+
+    betas: array_like
+        The defending indices for each team. The shape of this 
+        array will be [n_gameweeks, n_teams] such that betas[i,j] 
+        gives the defending index of team j in gameweek i.
+
+    gamma: float
+        The home advantage parameter. The shape of this array will 
+        be [n_gameweeks,], such that gamma[i] is the home advantage 
+        parameter in gameweek i.
+
+    rho: float
+        The correlation parameter for low-scoring games. The shape 
+        of this array will be [n_gameweeks,], such that rho[i] 
+        is the correlation parameter in gameweek i.
+    """
+
+    alpha_tot = np.zeros((36,20))
+    beta_tot = np.zeros((36,20))
+    gamma_tot = np.zeros(36)
+    rho_tot = np.zeros(36)
+
+    for i in np.arange(1,37):
         if i==1:
             alpha_tot[0,:],beta_tot[0,:],gamma_tot[0],rho_tot[0] = fit_model_up_to_round(data)
         else:
@@ -166,6 +424,48 @@ def fit_model_season(data):
     return alpha_tot,beta_tot,gamma_tot,rho_tot
 
 def model(x,y,alpha_i,alpha_j,beta_i,beta_j,gamma,rho):
+
+    """
+    The basic probability model of Dixon and Coles. Computes the 
+    probability of the home team scoring x goals and the away team 
+    scoring y goals given the appropriate model parameters. Note 
+    that if the inputs are arrays, they must all have the same shape.
+
+    Arguments
+    ---------
+
+    x: float or array_like
+        Home team goals.
+
+    y: float or array_like
+        Away team goals. 
+
+    alpha_i: float or array_like
+        Home team attacking indices.
+
+    alpha_j: float or array_like
+        Away team attacking indices.
+
+    beta_i: float or array_like
+        Home team defending indices.
+
+    beta_j: float or array_like
+        Away team defending indices.
+
+    gamma: float or array_like
+        Home team advantage parameters.
+
+    rho: float or array_like
+        Correlation parameters.
+
+    Returns
+    -------
+
+    probability: float or array_like
+        The probability of the home team scoring x goals and the 
+        away team scoring y goals.
+
+    """
 
     lambda_k = alpha_i*beta_j*gamma
     mu_k = alpha_j*beta_i
@@ -198,6 +498,49 @@ def model(x,y,alpha_i,alpha_j,beta_i,beta_j,gamma,rho):
     return correlations*np.exp(-lambda_k)*lambda_k**x*np.exp(-mu_k)*mu_k**y / (factorial(x)*factorial(y))
 
 def result_probabilities(alpha_i,alpha_j,beta_i,beta_j,gamma,rho):
+
+    """
+    The probabilities of home wins, draws and away wins given a set of 
+    model parameters. Computed by constructing a grid of home and away 
+    goals between 0 and 10 and summing over the appropriate elements. 
+    Note that this is not vectorized, so that one must loop over different 
+    sets of parameters in order to obtain probabilities for different 
+    matches.
+
+    Arguments
+    ---------
+
+    alpha_i: float
+        Home team attacking indices.
+
+    alpha_j: float
+        Away team attacking indices.
+
+    beta_i: float
+        Home team defending indices.
+
+    beta_j: float
+        Away team defending indices.
+
+    gamma: float
+        Home team advantage parameters.
+
+    rho: float
+        Correlation parameters.    
+
+    Returns
+    -------
+
+    home_win: float
+        The predicted probability of a home win.
+
+    draw: float
+        The predicted probability of a draw.
+
+    away_win: float
+        The predicted probability of an away win.
+
+    """
  
     n_goals = np.arange(0,11)
     x,y = np.meshgrid(n_goals,n_goals,indexing='ij')
@@ -210,7 +553,35 @@ def result_probabilities(alpha_i,alpha_j,beta_i,beta_j,gamma,rho):
 
 def result_likelihood(results,home_prob,draw_prob,away_prob):
 
-    #results = 0 (home), 1 (draw), 2 (away)
+    """
+    The predictive likelihood function (Equation 4.7 of Dixon and Coles) 
+    used to constrain the hyperparameter zeta.
+
+    Arguments
+    ---------
+
+    results: array_like
+        An array with elements with values 0, 1 or 2.
+        0 => home win
+        1 => draw 
+        2 => away win 
+
+    home_prob: array_like
+        The predicted probability of a home win. 
+
+    draw_prob: array_like
+        The predicted probability of a draw. 
+
+    away_prob: array_like
+        The predicted probability of an away win. 
+
+    Returns
+    -------
+
+    lnprob: float
+        The predictive log likelihood.
+
+    """
 
     lnprob = np.ones(results.shape)
     lnprob[results==0] = np.log(home_prob[results==0])
@@ -219,7 +590,34 @@ def result_likelihood(results,home_prob,draw_prob,away_prob):
 
     return np.sum(lnprob)
 
-def zeta_likelihood(zeta,data):
+def compute_zeta(data,zeta = np.linspace(0.,0.015,20) ):
+
+    """
+    Given some data, calculate the predictive log-likelihood for a 
+    grid of zeta values in order to find the optimal value. The data 
+    are divided into unique 'game days'. For each game day, the model 
+    is trained on all preceding game days, and the result probabilities 
+    stored. For each zeta, the predictive log-likelihood is then calculated.
+
+    Arguments
+    ---------
+
+    data: pandas.DataFrame
+        The data. 
+
+    zeta = (= numpy.linspace(0.,0.015,20) ) array_like
+        Range of zeta values to consider. 
+
+    Returns
+    -------
+
+    zeta: array_like
+        The zeta values considered.
+
+    lnprob: array_like
+        The predictive log-likelihood for each zeta.
+
+    """
 
     results = np.ones(len(data)).astype(int)
     x = data['HomeGoals'].values
@@ -228,39 +626,46 @@ def zeta_likelihood(zeta,data):
     results[x==y] = 1 
     results[x<y] = 2
 
-    unique_dates = np.unique(data.index.values)[50::2]
+    unique_dates = np.unique(data.index.values)[20::20]
+    lnprob = np.zeros_like(zeta)
 
-    home_probs = np.zeros(results.shape)
-    draw_probs = np.zeros(results.shape)
-    away_probs = np.zeros(results.shape)
-    for date in unique_dates:
-        print(date)
-        thisdate = data.index==date
-        training_thisdate = data.loc[data.index<date,:]
-        try:
-            params_guess = np.append(np.append(np.hstack((a[:19],b)),g),r)
-        except:
-            params_guess = np.ones(41)
-            params_guess[-1] = 0.
-        a,b,g,r = compute_parameters(training_thisdate,params_guess=params_guess,n_teams=20,date=date,zeta=zeta)
-        hometeams_thisdate = data['HomeTeam'].iloc[thisdate].values
-        awayteams_thisdate = data['AwayTeam'].iloc[thisdate].values
-        results_thisdate = results[thisdate]
-        home_probs_thisdate = np.zeros(results_thisdate.shape)
-        draw_probs_thisdate = np.zeros(results_thisdate.shape)
-        away_probs_thisdate = np.zeros(results_thisdate.shape)
-        a_home = np.array(a[hometeams_thisdate])
-        a_away = np.array(a[awayteams_thisdate])
-        b_home = np.array(b[hometeams_thisdate])
-        b_away = np.array(b[awayteams_thisdate])
-        for j in np.arange(len(hometeams_thisdate)):
-            home_probs_thisdate[j],draw_probs_thisdate[j],away_probs_thisdate[j] = result_probabilities(a_home[j],\
-                                                                                    a_away[j],b_home[j],b_away[j],g,r)
-        home_probs[thisdate] = home_probs_thisdate
-        draw_probs[thisdate] = draw_probs_thisdate
-        away_probs[thisdate] = away_probs_thisdate
+    for i,zi in enumerate(zeta):
+        if i!=0:
+            print('\033[F' + '\033[K' + '\033[F')
+        print("{} zeta values to go...".format(len(zeta) - i))
+        home_probs = np.zeros(results.shape)
+        draw_probs = np.zeros(results.shape)
+        away_probs = np.zeros(results.shape)
+        for date in unique_dates:
+            thisdate = data.index==date
+            training_thisdate = data.loc[data.index<date,:]
+            try:
+                params_guess = np.append(np.append(np.hstack((a[:19],b)),g),r)
+            except:
+                params_guess = np.ones(41)
+                params_guess[-1] = 0.
+            a,b,g,r = compute_parameters(training_thisdate,params_guess=params_guess,n_teams=20,date=date,zeta=zi)
+            hometeams_thisdate = data['HomeTeam'].iloc[thisdate].values
+            awayteams_thisdate = data['AwayTeam'].iloc[thisdate].values
+            results_thisdate = results[thisdate]
+            home_probs_thisdate = np.zeros(results_thisdate.shape)
+            draw_probs_thisdate = np.zeros(results_thisdate.shape)
+            away_probs_thisdate = np.zeros(results_thisdate.shape)
+            a_home = np.array(a[hometeams_thisdate])
+            a_away = np.array(a[awayteams_thisdate])
+            b_home = np.array(b[hometeams_thisdate])
+            b_away = np.array(b[awayteams_thisdate])
+            for j in np.arange(len(hometeams_thisdate)):
+                home_probs_thisdate[j],draw_probs_thisdate[j],away_probs_thisdate[j] = result_probabilities(a_home[j],\
+                                                                                        a_away[j],b_home[j],b_away[j],g,r)
+            home_probs[thisdate] = home_probs_thisdate
+            draw_probs[thisdate] = draw_probs_thisdate
+            away_probs[thisdate] = away_probs_thisdate
+        ind = home_probs!=0.
+        home_probs,draw_probs,away_probs,res_thisdate = home_probs[ind],draw_probs[ind],away_probs[ind],results[ind]
+        lnprob[i] = result_likelihood(res_thisdate,home_probs,draw_probs,away_probs)
     
-    return home_probs[home_probs!=0.],draw_probs[home_probs!=0.],away_probs[home_probs!=0.],results[home_probs!=0.]
+    return zeta,lnprob
 
 
 
