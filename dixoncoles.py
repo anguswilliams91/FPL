5ef44994d4ee61923a67f6337e219abfb0cf913a
anguswilliams91 <anguswilliams91@gmail.com>
2017-04-21 11:53:28 +0100

    add new model that includes shots on target

--- dixoncoles.py
+++ dixoncoles.py
@@ -13,6 +13,7 @@ from scipy.misc import factorial
 from multiprocessing import pool
 
 def process_raw_data(raw,filestr='data/PL_2015-16.csv'):
+
     """Data downloaded from http://www.football-data.co.uk/englandm.php
 
     Arguments
@@ -33,8 +34,9 @@ def process_raw_data(raw,filestr='data/PL_2015-16.csv'):
 
     raw.loc[:,'Date'] = pd.to_datetime(raw.loc[:,'Date'],dayfirst=True)
     raw.rename(columns={'FTHG':'HomeGoals','FTAG':'AwayGoals','B365H':'BookiesHomeWin',\
-                        'B365D':'BookiesDraw', 'B365A':'BookiesAwayWin'},inplace=True)
-    raw = raw[['Date','HomeTeam','AwayTeam','HomeGoals','AwayGoals','BookiesHomeWin','BookiesDraw','BookiesAwayWin']]
+                        'B365D':'BookiesDraw', 'B365A':'BookiesAwayWin', \
+                        'HST': 'HomeST', 'AST': 'AwayST'},inplace=True)
+    raw = raw[['Date','HomeTeam','AwayTeam','HomeGoals','AwayGoals','HomeST','AwayST','BookiesHomeWin','BookiesDraw','BookiesAwayWin']]
     raw.loc[:,'BookiesHomeWin'] = 1./raw['BookiesHomeWin']
     raw.loc[:,'BookiesDraw'] = 1./raw['BookiesDraw']
     raw.loc[:,'BookiesAwayWin'] = 1./raw['BookiesAwayWin']
@@ -134,7 +136,7 @@ def log_likelihood(params,data,n_teams=20,zeta=0.003,date=None):
 
     """
     Compute the (negative) log likelihood for a given set of model 
-    parameters.
+    parameters in the basic Dixon and Coles model.
 
     Arguments
     ---------
@@ -191,6 +193,158 @@ def log_likelihood(params,data,n_teams=20,zeta=0.003,date=None):
         lnL = -np.sum( (-mu_k + y_k*np.log(mu_k) - lambda_k + x_k*np.log(lambda_k) + np.log(correlations))*np.exp(-dt.astype(float)*zeta) )
 
     return lnL
+
+def log_likelihood_shotsontarget(params,data,n_teams=20,zeta=0.003,date=None):
+
+    """
+    Compute the (negative) log likelihood for a given set of model 
+    parameters in the extended model.
+
+    Arguments
+    ---------
+
+    params: array_like
+        The model parameters.
+
+    data: pandas.DataFrame
+        The data.
+
+    n_teams: (=20) int
+        The number of teams in the league (20 for the PL).
+
+    zeta: float 
+        The hyperparameter that determines the importance of 
+        matches at time t in the past. The rate is in 1/days 
+        rather than 1/half-weeks as is done in Dixon and Coles.
+
+    date: string or datetime object 
+        The date for which the fit should be carried out.
+
+    Returns
+    -------
+
+    lnL: float
+        The negative log-likelihood. 
+
+    """
+
+    alphas = params[: n_teams-1 ]
+    betas = params[n_teams-1 : 2*n_teams - 1]
+    epsilons = params[2*n_teams - 1: 3*n_teams - 1]
+    gamma = params[-1]
+    reduced_params = np.append(np.append(np.hstack((alphas,betas)),gamma),0.)
+    x_k = data['HomeGoals'].values.astype('float') 
+    y_k = data['AwayGoals'].values.astype('float')
+    p_k = data['HomeST'].values.astype('float')
+    q_k = data['AwayST'].values.astype('float')
+    mu_k,lambda_k = compute_lambdas_mus(reduced_params,data,n_teams)
+
+    epsilons_ik  = epsilons[data['HomeTeam'].values]
+    epsilons_jk = epsilons[data['AwayTeam'].values]    
+
+    match_dates = data.index.values
+    if date is None:
+        prediction_date = data.index.values[-1]
+    else:
+        prediction_date = np.datetime64(pd.to_datetime(date))
+    dt = (prediction_date - match_dates).astype('timedelta64[D]')
+
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore",category=RuntimeWarning)
+        home_rate = x_k*np.log(epsilons_ik) + (p_k-x_k)*np.log(1.-epsilons_ik) - lambda_k + p_k*np.log(lambda_k)
+        away_rate = y_k*np.log(epsilons_jk) + (q_k-y_k)*np.log(1.-epsilons_jk) - mu_k + q_k*np.log(mu_k)
+        lnL = -np.sum( (home_rate + away_rate)*np.exp(-dt.astype(float)*zeta) )
+
+    return lnL
+
+def jacobian_shotsontarget(params,data,n_teams=20,zeta=0.003,date=None):
+
+    """
+    Compute the (negative) derivatives of the log likelihood for a given set of model 
+    parameters in the extended model.
+
+    Arguments
+    ---------
+
+    params: array_like
+        The model parameters.
+
+    data: pandas.DataFrame
+        The data.
+
+    n_teams: (=20) int
+        The number of teams in the league (20 for the PL).
+
+    zeta: float 
+        The hyperparameter that determines the importance of 
+        matches at time t in the past. The rate is in 1/days 
+        rather than 1/half-weeks as is done in Dixon and Coles.
+
+    date: string or datetime object 
+        The date for which the fit should be carried out.
+
+    Returns
+    -------
+
+    derivs: array_like
+        The (negative) derivatives of the log-likelihood with respect 
+        to the parameters. 
+
+    """
+
+    deriv_alphas = np.zeros(n_teams - 1)
+    deriv_betas = np.zeros(n_teams)
+    deriv_epsilons = np.zeros(n_teams)
+
+    alphas = params[: n_teams-1 ]
+    betas = params[n_teams-1 : 2*n_teams - 1]
+    epsilons = params[2*n_teams - 1: 3*n_teams - 1]
+    gamma = params[-1]
+    reduced_params = np.append(np.append(np.hstack((alphas,betas)),gamma),0.)
+    x_k = data['HomeGoals'].values.astype('float') 
+    y_k = data['AwayGoals'].values.astype('float')
+    p_k = data['HomeST'].values.astype('float')
+    q_k = data['AwayST'].values.astype('float')
+    mu_k,lambda_k = compute_lambdas_mus(reduced_params,data,n_teams)
+
+    alphas = np.append(alphas,n_teams-np.sum(alphas))
+    alpha_ik = alphas[data['HomeTeam'].values]
+    alpha_jk = alphas[data['AwayTeam'].values]
+    beta_ik = betas[data['HomeTeam'].values]
+    beta_jk = betas[data['AwayTeam'].values]
+    epsilon_ik  = epsilons[data['HomeTeam'].values]
+    epsilon_jk = epsilons[data['AwayTeam'].values]
+
+    match_dates = data.index.values
+    if date is None:
+        prediction_date = data.index.values[-1]
+    else:
+        prediction_date = np.datetime64(pd.to_datetime(date))
+    dt = (prediction_date - match_dates).astype('timedelta64[D]')
+    ft_k = np.exp(-dt.astype(float)*zeta)
+
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore",category=RuntimeWarning)
+
+        for i in np.arange(n_teams):
+
+            try:
+                deriv_alphas[i] = np.sum( (ft_k*beta_jk*gamma*(p_k/lambda_k - 1.))[data['HomeTeam'].values == i] ) + \
+                                    np.sum( (ft_k*beta_ik*(q_k/mu_k - 1.))[data['AwayTeam'].values == i] )
+            except IndexError:
+                pass
+
+            deriv_betas[i] = np.sum( (ft_k*alpha_jk*(q_k/mu_k - 1.))[data['HomeTeam'].values == i] ) + \
+                                    np.sum( (ft_k*alpha_ik*gamma*(p_k/lambda_k - 1.))[data['AwayTeam'].values == i] )
+
+            deriv_epsilons[i] = np.sum( (ft_k*( x_k/epsilon_ik - (p_k - x_k)/(1.-epsilon_ik) ) )[data['HomeTeam'].values == i] ) + \
+                                    np.sum( (ft_k*( y_k/epsilon_jk - (q_k - y_k)/(1.-epsilon_jk) ) )[data['AwayTeam'].values == i] )
+
+
+        deriv_gamma = np.sum(ft_k*alpha_ik*beta_jk*(p_k/lambda_k -1.))
+
+    return -np.append(np.hstack((deriv_alphas,deriv_betas,deriv_epsilons)),deriv_gamma)
+
 
 def rho_constraint_low(params,data,n_teams):
 
@@ -255,7 +409,7 @@ def rho_constraint_high(params,data,n_teams):
     return upper_bound - params[-1]
 
 
-def compute_parameters(data,params_guess=None,n_teams=20,date=None,zeta=0.003):
+def compute_parameters(data,model='basic',params_guess=None,n_teams=20,date=None,zeta=0.003):
 
     """
     Compute the parameters of the model given some data.
@@ -265,6 +419,11 @@ def compute_parameters(data,params_guess=None,n_teams=20,date=None,zeta=0.003):
 
     data: pandas.DataFrame
         The data.
+
+    model: string
+        The name of the model to fit. Either 'basic' for the 
+        original Dixon & Coles model, or 'shots' for my extension 
+        that models shots on target.
 
     params_guess: (= None) array_like
         Guesses for the model parameters. If None, then all the parameters 
@@ -284,49 +443,70 @@ def compute_parameters(data,params_guess=None,n_teams=20,date=None,zeta=0.003):
     Returns
     -------
 
-    alphas: array_like
-        The attacking indices for each team.
-
-    betas: array_like
-        The defending indices for each team.
-
-    gamma: float
-        The home advantage parameter.
-
-    rho: float
-        The correlation parameter for low-scoring 
-        games. 
+    parameters: tuple of arrays
+        If model == 'basic' then (alphas,betas,gamma,rho) or if 
+        model == 'shots' then (alphas,betas,epsilons,gamma).
 
     """
 
-    if params_guess is None:
-        params_guess = np.ones(2*n_teams + 1)
-        params_guess[-1] = 0.
+    if model == 'basic':
 
-    cons = ({'type': 'ineq', 'fun': lambda x:  n_teams - np.sum(x[: n_teams - 1]) },\
-            {'type': 'ineq', 'fun': rho_constraint_low, 'args': (data,n_teams) },\
-            {'type': 'ineq', 'fun': rho_constraint_high, 'args': (data,n_teams) } )
+        if params_guess is None:
+            params_guess = np.ones(2*n_teams + 1)
+            params_guess[-1] = 0.
 
-    bnds = ((0.,None),)*2*n_teams
-    bnds = bnds + ((-1.,1.),)
+        cons = ({'type': 'ineq', 'fun': lambda x:  n_teams - np.sum(x[: n_teams - 1]) },\
+                {'type': 'ineq', 'fun': rho_constraint_low, 'args': (data,n_teams) },\
+                {'type': 'ineq', 'fun': rho_constraint_high, 'args': (data,n_teams) } )
 
-    res = minimize(log_likelihood, params_guess, args=(data,n_teams,zeta,date), jac=None, \
-            method='SLSQP', bounds=bnds, constraints=cons, options={'maxiter': 200})
+        bnds = ((0.,None),)*2*n_teams
+        bnds = bnds + ((-1.,1.),)
 
-    if res.success != True:
-        Exception('Failed to optimize the model.')
+        res = minimize(log_likelihood, params_guess, args=(data,n_teams,zeta,date), jac=None, \
+                method='SLSQP', bounds=bnds, constraints=cons, options={'maxiter': 200})
 
-    alphas = res.x[:n_teams - 1]
-    alphas = np.append(alphas, n_teams - np.sum(alphas))
+        if res.success != True:
+            Exception('Failed to optimize the model.')
 
-    betas = res.x[n_teams - 1 : 2*n_teams - 1]
-    gamma = res.x[-2]
-    rho = res.x[-1]
+        alphas = res.x[:n_teams - 1]
+        alphas = np.append(alphas, n_teams - np.sum(alphas))
 
-    return alphas,betas,gamma,rho
+        betas = res.x[n_teams - 1 : 2*n_teams - 1]
+        gamma = res.x[-2]
+        rho = res.x[-1]
+
+        return alphas,betas,gamma,rho
+
+    elif model == 'shots':
+
+        if params_guess is None:
+            params_guess = np.ones(3*n_teams)
+            params_guess[2*n_teams - 1: 3*n_teams - 1] = 0.2
+
+        cons = ({'type': 'ineq', 'fun': lambda x:  n_teams - np.sum(x[: n_teams - 1]) })
+
+        bnds_ab = ((0.,None),)*(2*n_teams - 1)
+        bnds_epsilon = ((0.,1.),)*n_teams
+        bnds_gamma = ((0.,None),)
+        bnds = bnds_ab + bnds_epsilon + bnds_gamma
+
+        res = minimize(log_likelihood_shotsontarget, params_guess, args=(data,n_teams,zeta,date), jac=jacobian_shotsontarget, \
+                method='SLSQP', bounds=bnds, constraints=cons, options={'maxiter': 500})
+
+        if res.success != True:
+            Exception('Failed to optimize the model.')
+
+        alphas = res.x[:n_teams - 1]
+        alphas = np.append(alphas, n_teams - np.sum(alphas))
+
+        betas = res.x[n_teams - 1 : 2*n_teams - 1]
+        epsilons = res.x[2*n_teams - 1: 3*n_teams - 1]
+        gamma = res.x[-1]
+
+        return alphas,betas,epsilons,gamma
 
 
-def fit_model_up_to_round(data,gameweek=1,window=10):
+def fit_model_up_to_round(data,model='basic',gameweek=1,window=10):
 
     """
     Fit the model up to a given 'round', defined by the Gameweek column 
@@ -339,6 +519,11 @@ def fit_model_up_to_round(data,gameweek=1,window=10):
     data: pandas.DataFrame
         The data.
 
+    model: string
+        The name of the model to fit. Either 'basic' for the 
+        original Dixon & Coles model, or 'shots' for my extension 
+        that models shots on target.
+
     gameweek: (=1) int
         The gameweek (1<= gameweek <= 36) up to which the model should be fit. Only the ten 
         weeks before the selected gameweek will be considered.
@@ -349,32 +534,26 @@ def fit_model_up_to_round(data,gameweek=1,window=10):
     Returns
     -------
 
-    alphas: array_like
-        The attacking indices for each team.
-
-    betas: array_like
-        The defending indices for each team.
-
-    gamma: float
-        The home advantage parameter.
-
-    rho: float
-        The correlation parameter for low-scoring 
-        games. 
+    parameters: tuple of arrays
+        If model == 'basic' then (alphas,betas,gamma,rho) or if 
+        model == 'shots' then (alphas,betas,epsilons,gamma).
 
     """
 
-    params = np.ones(41)
-    params[-1] = 0.
+    if model == 'basic':
+        params = np.ones(41)
+        params[-1] = 0.
+    elif model == 'shots':
+        params = np.ones(60)
+        params[2*20 - 1: 3*20 - 1] = 0.2
     if gameweek<window:
         train = data.loc[data['Gameweek'] <= gameweek, :]
     else:
         train = data.loc[(data['Gameweek'] <= gameweek)&(data['Gameweek'] > gameweek - window), :]
-    res = compute_parameters(train,params_guess=params,n_teams=20,zeta=0.)
-    alphas,betas,gamma,rho = res
-    return alphas,betas,gamma,rho
+    res = compute_parameters(train,model=model,params_guess=params,n_teams=20,zeta=0.)
+    return res
 
-def fit_model_season(data,window=10):
+def fit_model_season(data,model='basic',window=10):
 
     """
     Fit the model for the whole season, computing the parameters for each gameweek. Note 
@@ -387,6 +566,11 @@ def fit_model_season(data,window=10):
     data: pandas.DataFrame
         The data.
 
+    model: string
+        The name of the model to fit. Either 'basic' for the 
+        original Dixon & Coles model, or 'shots' for my extension 
+        that models shots on target.
+
     window: (=10) int
         The number of previous gameweeks to consider when fitting the model up 
         to a given gameweek.
@@ -394,42 +578,50 @@ def fit_model_season(data,window=10):
     Returns
     -------
 
-    alphas: array_like
-        The attacking indices for each team. The shape of this 
-        array will be [n_gameweeks, n_teams] such that alphas[i,j] 
-        gives the attacking index of team j in gameweek i.
-
-    betas: array_like
-        The defending indices for each team. The shape of this 
-        array will be [n_gameweeks, n_teams] such that betas[i,j] 
-        gives the defending index of team j in gameweek i.
-
-    gamma: float
-        The home advantage parameter. The shape of this array will 
-        be [n_gameweeks,], such that gamma[i] is the home advantage 
-        parameter in gameweek i.
-
-    rho: float
-        The correlation parameter for low-scoring games. The shape 
-        of this array will be [n_gameweeks,], such that rho[i] 
-        is the correlation parameter in gameweek i.
+    parameters: tuple of arrays
+        If model == 'basic' then (alphas,betas,gamma,rho) or if 
+        model == 'shots' then (alphas,betas,epsilons,gamma). The arrays 
+        have shape [n_gameweeks, n_teams] (or [n_gameweeks, 1] for gamma 
+        or rho, which are not specific to any team).
     """
 
-    alpha_tot = np.zeros((36,20))
-    beta_tot = np.zeros((36,20))
-    gamma_tot = np.zeros(36)
-    rho_tot = np.zeros(36)
 
-    for i in np.arange(1,37):
-        if i==1:
-            alpha_tot[0,:],beta_tot[0,:],gamma_tot[0],rho_tot[0] = fit_model_up_to_round(data,window=window)
-        else:
-            try:
-                alpha_tot[i-1,:],beta_tot[i-1,:],gamma_tot[i-1],rho_tot[i-1] = fit_model_up_to_round(data,gameweek=i,window=window)
-            except:
-                alpha_tot[i-1,:],beta_tot[i-1,:],gamma_tot[i-1],rho_tot[i-1] = alpha_tot[i-2,:],beta_tot[i-2,:],gamma_tot[i-2],rho_tot[i-2]
 
-    return alpha_tot,beta_tot,gamma_tot,rho_tot
+    if model == 'basic':
+
+        alpha_tot = np.zeros((36,20))
+        beta_tot = np.zeros((36,20))
+        gamma_tot = np.zeros(36)
+        rho_tot = np.zeros(36)
+
+        for i in np.arange(1,37):
+            if i==1:
+                alpha_tot[0,:],beta_tot[0,:],gamma_tot[0],rho_tot[0] = fit_model_up_to_round(data,model='basic',window=window)
+            else:
+                try:
+                    alpha_tot[i-1,:],beta_tot[i-1,:],gamma_tot[i-1],rho_tot[i-1] = fit_model_up_to_round(data,model='basic',gameweek=i,window=window)
+                except:
+                    alpha_tot[i-1,:],beta_tot[i-1,:],gamma_tot[i-1],rho_tot[i-1] = alpha_tot[i-2,:],beta_tot[i-2,:],gamma_tot[i-2],rho_tot[i-2]
+
+        return alpha_tot,beta_tot,gamma_tot,rho_tot
+
+    elif model == 'shots':
+
+        alpha_tot = np.zeros((36,20))
+        beta_tot = np.zeros((36,20))
+        epsilon_tot = np.zeros((36,20))
+        gamma_tot = np.zeros(36)
+
+        for i in np.arange(1,37):
+            if i==1:
+                alpha_tot[0,:],beta_tot[0,:],epsilon_tot[0,:],gamma_tot[0] = fit_model_up_to_round(data,model='shots',window=window)
+            else:
+                try:
+                    alpha_tot[i-1,:],beta_tot[i-1,:],epsilon_tot[i-1,:],gamma_tot[i-1] = fit_model_up_to_round(data,model='shots',gameweek=i,window=window)
+                except:
+                    alpha_tot[i-1,:],beta_tot[i-1,:],epsilon_tot[i-1,:],gamma_tot[i-1] = alpha_tot[i-2,:],beta_tot[i-2,:],epsilon_tot[i-2,:],gamma_tot[i-2]
+
+        return alpha_tot,beta_tot,epsilon_tot,gamma_tot        
 
 def model(x,y,alpha_i,alpha_j,beta_i,beta_j,gamma,rho):
 
